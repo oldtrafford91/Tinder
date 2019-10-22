77f77f5b753e8bc2ebaa05c2468e34ceddb47d18
Hai Tran <tuanhai.dev@gmail.com>
2019-10-22 11:02:09 +0700

    Animate `CardView` out of screen when pan gesture translation  exceed threshold

--- Tinder/CardView.swift
+++ Tinder/CardView.swift
@@ -1,6 +1,13 @@
 import UIKit
 
+private enum CardDismissDirection: CGFloat {
+  case right = 1
+  case left = -1
+}
+
 class CardView: UIView {
+  // MARK: Constant
+  private let threshold: CGFloat = 100
   
   // MARK: - Subviews
   let imageView = UIImageView(image: #imageLiteral(resourceName: "kelly3"))
@@ -19,6 +26,7 @@ class CardView: UIView {
   private func configure() {
     layer.cornerRadius = 12
     clipsToBounds = true
+    translatesAutoresizingMaskIntoConstraints = false
     
     addSubview(imageView)
     imageView.fillSuperview()
@@ -33,7 +41,7 @@ class CardView: UIView {
     case .changed:
       handleChanged(gesture)
     case .ended:
-      handleEndedGesture()
+      handleEndedGesture(gesture)
     default:
       ()
     }
@@ -41,10 +49,17 @@ class CardView: UIView {
   
   private func handleChanged(_ gesture: UIPanGestureRecognizer) {
     let translation = gesture.translation(in: self)
-    transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    let degree: CGFloat = translation.x / 20
+    let angle: CGFloat = degree * .pi / 180
+    let rotationTransform = CGAffineTransform(rotationAngle: angle)
+    
+    transform = rotationTransform.translatedBy(x: translation.x, y: translation.y)
   }
   
-  private func handleEndedGesture() {
+  private func handleEndedGesture(_ gesture: UIPanGestureRecognizer) {
+    let translationDirection: CardDismissDirection = gesture.translation(in: self).x > 0 ? .right : .left
+    let shouldDismissCard = abs(gesture.translation(in: self).x) > threshold
+
     UIView
       .animate(withDuration: 0.75,
                    delay: 0,
@@ -52,9 +67,14 @@ class CardView: UIView {
                    initialSpringVelocity: 0.1,
                    options: .curveEaseOut,
                    animations: {
-                    self.transform = .identity
-      }, completion: { _ in
+                    if shouldDismissCard {
+                      self.transform = CGAffineTransform(translationX: 600 * translationDirection.rawValue , y: 0)
+                    } else {
+                      self.transform = .identity
+                    }
 
+      }, completion: { _ in
+        self.transform = .identity
       })
   }
   
