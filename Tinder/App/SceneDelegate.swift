18fa52a5172939f8a6d35e75424d3acc375eb602
Hai Tran <tuanhai.dev@gmail.com>
2019-11-03 11:52:56 +0700

    Import firebase configuration file

--- Tinder/App/SceneDelegate.swift
+++ Tinder/App/SceneDelegate.swift
@@ -1,10 +1,12 @@
 import UIKit
+import Firebase
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    FirebaseApp.configure()
     guard let _ = (scene as? UIWindowScene) else { return }
     guard let windowScene = (scene as? UIWindowScene) else { return }
     window = UIWindow(windowScene: windowScene)
